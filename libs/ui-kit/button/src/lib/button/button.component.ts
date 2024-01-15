20a8d289cd184d6bf6b928ca4ba53550983e2d76
rkhizriev <r.s.khizriev@tmn3.etagi.com>
2024-01-15 14:40:29 +0500

    ui-kit button init

---
+++ libs/ui-kit/button/src/lib/button/button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nx-storybook-test-2-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+})
+export class ButtonComponent {}
