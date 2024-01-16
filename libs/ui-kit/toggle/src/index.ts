9c84fa617af35fb8d7b2fe03f3839dde5fb9f744
rkhizriev <r.s.khizriev@tmn3.etagi.com>
2024-01-16 11:36:36 +0500

    add toggle component

--- libs/ui-kit/toggle/src/index.ts
+++ libs/ui-kit/toggle/src/index.ts
@@ -1,1 +1,2 @@
-export * from './lib/toggle/toggle.component';
+export * from './lib/toggle.component';
+export * from './lib/toggle.module';
---
+++ libs/ui-kit/toggle/src/lib/toggle.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { ToggleComponent } from './toggle.component';
+
+@NgModule({
+	declarations: [ToggleComponent],
+	imports: [CommonModule],
+	exports: [ToggleComponent],
+})
+export class ToggleModule {}
