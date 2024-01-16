18dc6fc7fa8a39ea658c538cc85ce492bada67eb
rkhizriev <r.s.khizriev@tmn3.etagi.com>
2024-01-16 17:09:41 +0500

    join stylesheet

--- apps/nx-storybook-test-2/src/app/app.component.ts
+++ apps/nx-storybook-test-2/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { ButtonModule } from '@nx-storybook-test-2/button';
+import { ToggleModule } from '@nx-storybook-test-2/toggle';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, ButtonModule, ToggleModule],
   selector: 'nx-storybook-test-2-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
