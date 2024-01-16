9804c6843abfe11c8a4d47c04a5b9a9d753fa02e
rkhizriev <r.s.khizriev@tmn3.etagi.com>
2024-01-16 16:51:54 +0500

    create button lib in ui-kit lib

---
+++ libs/ui-kit/src/lib/button/src/lib/types/button.model.ts
@@ -0,0 +1,11 @@
+export type ButtonColor =
+	| 'primary'
+	| 'secondary'
+	| 'error'
+	| 'fade'
+	| 'default'
+	| 'transparent'
+	| 'transparent-grey'
+	| 'transparent-error';
+
+export type ButtonSize = 'md' | 'sm';
